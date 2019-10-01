99b64aa9f56c99b8dc1b2fb6503e8034bc62d168
Don <don.vu1212@gmail.com>
2019-09-30 17:13:33 -0700

    Use HTMLUnit library to fetch recipe website

--- recipe-fetcher/src/main/java/com/mycompany/recipe/fetcher/Main.java
+++ recipe-fetcher/src/main/java/com/mycompany/recipe/fetcher/Main.java
@@ -6,12 +6,26 @@
 
 package com.mycompany.recipe.fetcher;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 /**
  *
  * @author don
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+        try {
+            String searchUrl = "https://www.bettycrocker.com/"
+                    + "menus-holidays-parties/mhplibrary/recipes/"
+                    + "heirloom-recipes-that-are-tried-and-true";
+            
+            HtmlPage page = client.getPage(searchUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
